931b0a3785fb2287e3cca694d7ddca8800daf963
Adam <adam.walczak@vip.onet.pl>
2020-11-10 11:45:41 +0100

    Pierwszy commit

---
+++ Project-Library/src/main/java/pl/atw/demo/ProjectLibraryApplication.java
@@ -0,0 +1,13 @@
+package pl.atw.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectLibraryApplication.class, args);
+	}
+
+}
---
+++ Project-Library/src/test/java/pl/atw/demo/ProjectLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.atw.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
